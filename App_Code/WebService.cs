c86f951ba13b1df2c6b8843d0f83a6f4b302d833
Chloe-code <61135771+Chloe-code@users.noreply.github.com>
2020-12-30 16:42:15 +0800

    Add files via upload

--- App_Code/WebService.cs
+++ App_Code/WebService.cs
@@ -1,1 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
 
+/// <summary>
+/// WebService 的摘要描述
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //如果使用設計的元件，請取消註解下列一行
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
