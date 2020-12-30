90416582bd65b23173391739fbc04ddb91daf559
Chloe-code <61135771+Chloe-code@users.noreply.github.com>
2020-12-30 16:43:43 +0800

    Add files via upload

---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        Response.ContentType = "image/JPEG"; //Application/pdf
+        //Get the physical path to the file.
+        string FilePath = MapPath("post.jpg");
+        //Write the file directly to the HTTP content output stream.
+        Response.WriteFile(FilePath);
+        Response.End();
+    }
+}
